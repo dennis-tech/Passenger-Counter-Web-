ffaf4de1213e8440ed2f6dfc7a3ee4f7398b32e2
dennis-tech <irungudennis956@yahoo.com>
2022-03-27 13:37:47 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+let count = 0;
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    let ver = count + " - ";
+    saveEl.textContent += ver;
+    countEl.textContent = 0;
+    count = 0;
+}
+function reset (){
+    
+    countEl.textContent = 0;
+    saveEl.textContent  = 0 + "-";    
+}
+
+console.log("Let's count people on the subway!");
